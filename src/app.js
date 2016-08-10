be769c30594304a4b673ca5cc8f569dcf0b92e4f
Mateus Jabour <mjabourbp@gmail.com>
2016-08-10 18:12:36 -0400

    Marks up the app and splits components

--- src/app.js
+++ src/app.js
@@ -1,9 +1,35 @@
 'use strict'
 
 import React from 'react'
+import Search from './components/search'
+import UserInfo from './components/user-info'
+import Actions from './components/actions'
+import Repos from './components/repos'
 
 const App = () => (
-  <div>My App</div>
+  <div className='app'>
+
+    <Search />
+    <UserInfo />
+    <Actions />
+
+    <Repos
+      className='repos'
+      title='Repositórios'
+      repos={[{
+        name: 'Nome do repo',
+        link: '#'
+      }]} />
+
+    <Repos
+      className='starred'
+      title='Favoritos'
+      repos={[{
+        name: 'Nome do repo',
+        link: '#'
+      }]} />
+
+  </div>
 )
 
 export default App
---
+++ src/components/user-info.js
@@ -0,0 +1,23 @@
+'use strict'
+
+import React from 'react'
+
+const UserInfo = () => (
+  <div className='user-info'>
+
+    <img src='https://avatars.githubusercontent.com/u/11710000?v=3' />
+    <h1 className='username'>
+      <a href='https://github.com/MateusJabour'>Mateus Jabour</a>
+    </h1>
+
+    <ul className='repos-info'>
+      <li>Repositorios: 122</li>
+      <li>Seguidores: 10</li>
+      <li>Seguindo: 10</li>
+    </ul>
+
+  </div>
+
+)
+
+export default UserInfo
